3bb101c8cadeceee21aae958cc769dc2caeb3239
Lochlan Odlum Macbook <lodlum@me.com>
2022-01-05 21:28:27 +0000

    small  changes to orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -6,18 +6,20 @@ const User = require('../models/user');
 const Order = require('../models/order');
 const OrderItem = require('../models/order-item');
 
-//Need to limit products to not return any not avaliable for purchase
 exports.getProducts = async (req, res, next) => {
-  const products = await Product.findAll({ where: { isAvaliableForPurchase: true } });
+  const products = await Product.findAll({
+    where: { isAvaliableForPurchase: true },
+  });
 
   res.json(products);
 };
 
-//Need to limit products to only return those avaliable for purchase
 exports.getProduct = async (req, res, next) => {
   const { id } = req.params;
 
-  const product = await Product.findOne({ where: { id, isAvaliableForPurchase: true } });
+  const product = await Product.findOne({
+    where: { id, isAvaliableForPurchase: true },
+  });
 
   if (!product) {
     const error = new Error('Could not find a product with this id');
@@ -31,26 +33,32 @@ exports.getProduct = async (req, res, next) => {
 //Recieve order = {itemIds: [id1, id2, ...], stripeReceiptEmail?}
 //Starts an order, creates paymentIntent but still needs to be paid.
 exports.startOrder = async (req, res, next) => {
-  const { stripeReceiptEmail, itemIds } = req.body;
+  const { itemIds } = req.body;
 
-  const products = await Product.findAll({ where: { id: { [Op.or]: itemIds }, isAvaliableForPurchase: true } });
+  const products = await Product.findAll({
+    where: { id: { [Op.or]: itemIds }, isAvaliableForPurchase: true },
+  });
 
   if (products.length !== itemIds.length) {
     console.log(products);
-    const error = new Error('Could not match each item Id to an existing unique product.');
+    const error = new Error(
+      'Could not match each item Id to an existing unique product.'
+    );
     error.statusCode = 400;
     return next(error);
   }
 
   //Get total cost of order from products using order items.
 
-  const totalCostInPence = 100 * products.reduce((accumulator, current) => accumulator + +current.price, 0);
+  const totalCostInPence =
+    100 *
+    products.reduce((accumulator, current) => accumulator + +current.price, 0);
 
   //Create payment Intent
 
   const paymentIntent = await stripe.paymentIntents.create({
     amount: totalCostInPence,
-    receipt_email: stripeReceiptEmail,
+    // receipt_email: stripeReceiptEmail,
     currency: 'gbp',
   });
 
@@ -64,30 +72,32 @@ exports.startOrder = async (req, res, next) => {
     totalPrice: totalCostInPence / 100,
   });
 
-  const promises = [];
+  // const promises = [];
 
   //Reserve products so other users cannot buy a product which someone else is buying
-  products.forEach((product) => {
-    product.isReserved = true;
-    promises.push(product.save());
-    promises.push(
-      order.createOrderItem({
-        title: product.title,
-        description: product.description,
-        price: product.price,
-        rawImageKey: product.rawImageKey,
-        productId: product.id,
-      })
-    );
-  });
+  // products.forEach((product) => {
+  //   product.isReserved = true;
+  //   promises.push(product.save());
+  //   promises.push(
+  //     order.createOrderItem({
+  //       title: product.title,
+  //       description: product.description,
+  //       price: product.price,
+  //       rawImageKey: product.rawImageKey,
+  //       productId: product.id,
+  //     })
+  //   );
+  // });
 
-  await Promise.all(promises);
+  // await Promise.all(promises);
 
-  //This will unreserve products if order hasn't been paid in 30 mins and also cancels order and payment Intent so they cannot be charged after.
+  //If order hasn't been paid in 30 mins and also cancels order and payment Intent so they cannot be charged after.
 
   setTimeout(async () => {
     try {
-      const upToDatePaymentIntent = await stripe.paymentIntents.retrieve(paymentIntent.id);
+      const upToDatePaymentIntent = await stripe.paymentIntents.retrieve(
+        paymentIntent.id
+      );
 
       //Alternatively could just call order.reload() to reload existing order model instance
       const upToDateOrder = await Order.findOne({ where: { id: order.id } });
@@ -104,18 +114,20 @@ exports.startOrder = async (req, res, next) => {
 
       upToDateOrder.destroy();
 
-      products.forEach((product) => {
-        if (!product.isPurchased) {
-          product.isAvaliableForPurchase = true;
-          product.save();
-        }
-      });
+      // products.forEach((product) => {
+      //   if (!product.isPurchased) {
+      //     product.isAvaliableForPurchase = true;
+      //     product.save();
+      //   }
+      // });
     } catch (error) {
       console.error(error);
     }
   }, 1000 * 60 * 30);
 
-  res.status(200).json({ clientSecret: paymentIntent.client_secret });
+  res
+    .status(200)
+    .json({ clientSecret: paymentIntent.client_secret, orderId: order.id });
 };
 
 exports.getMyOrder = async (req, res, next) => {
@@ -123,7 +135,10 @@ exports.getMyOrder = async (req, res, next) => {
   const userId = req.userId;
 
   //Finding order directly rather than using user.getOrder(...), so custom error message for when order exists but not belonging to authenticated user.
-  const order = await Order.findOne({ where: { id: orderId, isPaymentCompleted: true }, include: [User, OrderItem] });
+  const order = await Order.findOne({
+    where: { id: orderId, isPaymentCompleted: true },
+    include: [User, OrderItem],
+  });
 
   if (!order) {
     const error = new Error('Could not find an order with this id');
@@ -151,7 +166,10 @@ exports.getMyOrders = async (req, res, next) => {
   const { orderIds } = req.body;
   const user = req.user;
 
-  const orders = await user.getOrders({ include: OrderItem, where: { isPaymentCompleted: true } });
+  const orders = await user.getOrders({
+    include: OrderItem,
+    where: { isPaymentCompleted: true },
+  });
 
   const ordersReturn = orders.map((order) => ({
     id: order.id,
