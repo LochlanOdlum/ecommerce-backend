20c9ca52e48c736053721448dd902edbe0f5377a
Lochlan Odlum Macbook <lodlum@me.com>
2023-12-08 22:55:38 +0000

    Update db dialect to mssql

--- util/database.js
+++ util/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USERNAME,
   process.env.DATABASE_PASSWORD,
   {
-    dialect: 'mysql',
+    dialect: 'mssql',
     host: process.env.DATABASE_HOST,
     dialectOptions: {
       ssl: {
